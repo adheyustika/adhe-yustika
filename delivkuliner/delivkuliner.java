b5c238fb23fc19b1628fc367db75ac5af86cab1f
adeyustika <adheyustikaandi@gmail.com>
2019-08-07 15:41:46 +0700

    first commit

---
+++ delivkuliner/delivkuliner.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kuliner;
+
+/**
+ *
+ * @author acer
+ */
+public class delivkuliner {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        awalmenu awalmenu = new awalmenu(); // penampilan class lain
+        awalmenu.setVisible(true); // tampilan gui dari class awal menu
+        
+        // TODO code application logic here
+    }
+    
+}
